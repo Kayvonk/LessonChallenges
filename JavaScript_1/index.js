8245538e7b5f9cc1bafad7f47474433676e3d656
Kayvon Kazemini <kayvonk@gmail.com>
2024-11-29 17:08:51 -0700

    adding challenge 1

---
+++ JavaScript_1/index.js
@@ -0,0 +1,11 @@
+numberArray = [1, 2, 3, 4, 5]
+
+// TODO 1
+// Write a for loop which loops through the numberArray 
+// and console logs the sum of all of the numbers combined
+
+
+
+// TODO 2
+// Write a for loop which loops through the numbersArray
+// and console logs only the odd numbers
